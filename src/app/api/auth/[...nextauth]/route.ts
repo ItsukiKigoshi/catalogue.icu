00ea75dc3c38a57e9d63cc2116d53bf6780b6cd4
ItsukiKigoshi <itsukikigoshi@gmail.com>
2024-11-25 16:38:10 +0900

    feat: add Login button
    Signed-off-by: ItsukiKigoshi <itsukikigoshi@gmail.com>

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,3 @@
-import { handlers } from "@/auth"
-export const { GET, POST } = handlers
+import { handlers } from "@/src/auth";
+
+export const { GET, POST } = handlers;
--- src/components/ButtonLogin.tsx
+++ src/components/ButtonLogin.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import { Button } from "@mantine/core";
+import Image from "next/image";
+import { signIn, signOut } from "next-auth/react";
+import { auth } from "@/src/auth";
 
-function ButtonLogin() {
-  return <Button variant="filled">Login</Button>;
+export default async function ButtonLogin() {
+  const session = await auth();
+  if (!session?.user) return null;
+  return (
+    <>
+      {session ? (
+        <>
+          <Image
+            src={session.user?.image ?? ""}
+            alt={session.user?.name ?? ""}
+          />
+          <Button
+            onClick={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            Logout
+          </Button>
+        </>
+      ) : (
+        <Button
+          onClick={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          Login
+        </Button>
+      )}
+    </>
+  );
 }
-
-export default ButtonLogin;
